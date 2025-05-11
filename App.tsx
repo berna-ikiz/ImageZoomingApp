735ffd5ce010a0350b418bd4d9da269b9caf744d
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-05-12 00:02:42 +0300

    Zoom functionality has been added.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import ZoomImage from './src/components/ZoomImage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ZoomImage/>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ZoomImage />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
 });
--- src/components/ZoomImage.tsx
+++ src/components/ZoomImage.tsx
@@ -1,19 +1,47 @@
-import { Animated, StyleSheet, Text, View } from 'react-native'
-import React, { useRef } from 'react'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 const ZoomImage = () => {
-    const scale = useRef(new Animated.Value(1)).current;
-  return (
-      <Animated.Image source={require('../../assets/cat-scream.jpg')} style={[styles.image,{transform:[{scale}]}]}/>
-  )
-}
+  const scale = useSharedValue(1);
+  const savedScale = useSharedValue(1);
 
-export default ZoomImage
+  const pinchGesture = Gesture.Pinch()
+    .onBegin(() => {
+      savedScale.value = scale.value;
+    })
+    .onUpdate((e) => {
+      scale.value = savedScale.value * e.scale;
+    })
+    .onEnd(() => {
+        scale.value = withSpring(1);
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  return (
+    <GestureDetector gesture={pinchGesture}>
+      <Animated.Image
+        source={require('../../assets/cat-scream.jpg')}
+        style={[styles.image, animatedStyle]}
+        resizeMode="contain"
+      />
+    </GestureDetector>
+  );
+};
 
 const styles = StyleSheet.create({
-    image:{
-        width:'100%',
-        height:300,
-        resizeMode:'contain'
-    }
-})
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default ZoomImage;
